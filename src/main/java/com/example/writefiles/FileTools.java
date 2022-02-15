b6cc4204fa46fbb6c278292a76bd22b6e127d77c
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-02-15 15:56:32 -0500

    uses a buffered reader instead of filewriter

--- src/main/java/com/example/writefiles/FileTools.java
+++ src/main/java/com/example/writefiles/FileTools.java
@@ -1,20 +1,22 @@
 package com.example.writefiles;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileTools {
 
-   static void writeFiles() {
+    static void writeFiles() {
         String path = "C:\\Users\\james.keogh\\IdeaProjects\\WriteFiles\\src\\main\\resources\\";
         String filename = "fromApp";
         String ext = ".txt";
         File fileOut = new File(path + filename + ext);
         try {
             FileWriter fileWriter = new FileWriter(fileOut);
-            fileWriter.write("Text to send into the file");
-            fileWriter.close();
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("Text to send into the file");
+            bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/com/example/writefiles/HelloController.java
+++ src/main/java/com/example/writefiles/HelloController.java
@@ -1,8 +1,5 @@
 package com.example.writefiles;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
